d8f9d1e64c2eb76cf3cf8ab911e06694687f1deb
MishataMSWira <nurjavierprasetyo8789@gmail.com>
2026-06-24 04:48:10 +0700

    idk man ngl

--- src/main/java/io/github/nurjavier8789/lateNightSMPPlugin2612/LateNightSMPPlugin2612.java
+++ src/main/java/io/github/nurjavier8789/lateNightSMPPlugin2612/LateNightSMPPlugin2612.java
@@ -1,16 +1,80 @@
 package io.github.nurjavier8789.lateNightSMPPlugin2612;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-public final class LateNightSMPPlugin2612 extends JavaPlugin {
+import net.kyori.adventure.text.Component;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.net.URI;
+
+public final class LateNightSMPPlugin2612 extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        startUIUpdater();
+
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("Late Night SMP Plugin is active!");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        event.joinMessage(Component.text("§a[+] §e" + player.getName()));
+
+        getServer().getScheduler().runTaskLater(this, () -> {
+            if (player.isOnline()) {
+                player.sendMessage("");
+                player.sendMessage("§b=======================================");
+                player.sendMessage("§eSelamat datang di Late Night SMP, §a" + player.getName() + "§e!");
+                player.sendMessage("§eSelamat bermain!");
+                player.sendMessage("§b=======================================");
+                player.sendMessage("");
+            }
+        }, 20L);
+    }
+
+    private void startUIUpdater() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    updateTabList(player);
+                }
+            }
+        }.runTaskTimer(this, 0L, 20L);
+    }
+
+
+    private void updateTabList(Player player) {
+        int ping = player.getPing();
+        String pingColor = "§a";
+        if (ping > 150 && ping <= 300) pingColor = "§e";
+        else if (ping > 300) pingColor = "§c";
+
+        Component header = Component.text("\n§b §lLate Night SMP§b\n    §7Selamat datang, §e" + player.getName() + "    \n");
+        Component footer = Component.text("\n§7Ping: " + pingColor + ping + "ms\n" + "§bHave fun playing!\n");
+
+        player.sendPlayerListHeaderAndFooter(header, footer);
+    }
+
+    @EventHandler
+    public void onPlayerQuitEvent(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        event.quitMessage(Component.text("§c[-] §e" + player.getName()));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Late Night SMP Plugin is disabled!");
+        getLogger().info("Have a nice day!");
     }
 }
